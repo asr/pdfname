c2a36490025169b57cd21a2e1f5ea6bc15c9882f
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2017-07-12 23:23:35 -0500

    [ test suite ] Ported the failing tests to tasty-silver.

--- Makefile
+++ Makefile
@@ -10,8 +10,9 @@ install-bin :
 
 .PHONY : succeed-tasty
 succeed-tasty :
-	cd test && ../dist/build/pdfname-tests/pdfname-tests
-	@echo "$@ succeeded!"
+	cd test && \
+	../dist/build/pdfname-tests/pdfname-tests --regex-include succeed
+	@echo "$@ succeeded-tasty!"
 
 # Tested with shelltestrunner 1.3.5.
 .PHONY : succeed-shelltestrunner
@@ -20,16 +21,13 @@ succeed-shelltestrunner :
 	          --execdir \
 	          --precise \
 	          test/succeed.test
-	@echo "$@ succeeded!"
+	@echo "$@ succeeded"
 
-# Tested with shelltestrunner 1.3.5.
 .PHONY : fail
 fail :
-	shelltest --color \
-	          --execdir \
-	          --precise \
-	          test/fail.test
-	@echo "$@ succeeded!"
+	cd test && \
+	../dist/build/pdfname-tests/pdfname-tests --regex-include fail
+	@echo "$@ succeeded"
 
 .PHONY : test
 test :
--- test/Main.hs
+++ test/Main.hs
@@ -13,13 +13,8 @@ import Test.Tasty.Silver.Interactive ( defaultMain )
 pdfnameBIN ∷ String
 pdfnameBIN = "../dist/build/pdfname/pdfname"
 
-succeedTests ∷ IO TestTree
-succeedTests = do
-  files ← findByExtension [".pdf"] "succeed"
-  return $ testGroup "Succeed" $ map oneSucceedTest files
-
-oneSucceedTest ∷ FilePath → TestTree
-oneSucceedTest testFile =
+tastyTest ∷ FilePath → TestTree
+tastyTest testFile =
   let goldenFile ∷ FilePath
       goldenFile = replaceExtension testFile ".golden"
 
@@ -28,10 +23,21 @@ oneSucceedTest testFile =
 
   in goldenVsProg testFile goldenFile pdfnameBIN args T.empty
 
+succeedTests ∷ IO TestTree
+succeedTests = do
+  files ← findByExtension [".pdf"] "succeed"
+  return $ testGroup "Succeed" $ map tastyTest files
+
+failTests ∷ IO TestTree
+failTests = do
+  files ← findByExtension [ ".pdf", ".txt" ] "fail"
+  return $ testGroup "Fail" $ map tastyTest files
+
 allTests ∷ IO TestTree
 allTests = do
-  tests ← succeedTests
-  return $ testGroup "Tests" [ tests ]
+  allSucceedTests ← succeedTests
+  allFailTests    ← failTests
+  return $ testGroup "Tests" [ allSucceedTests, allFailTests ]
 
 main ∷ IO ()
 main = defaultMain =<< allTests
